fb6ffffb116246bcd2aec86295f93b24dec0262a
Damian Mroczek <damianmroczek@yahoo.com>
2022-05-09 14:52:33 +0200

    buttons style, js counter

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,23 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const glassCounter = document.querySelector('.glass__counter--js');
+const addButton = document.querySelector('.button-add--js');
+const removeButton = document.querySelector('.button-remove--js');
+
+let currentGlassCounter = 0;
+
+addButton.addEventListener('click', () => {
+    currentGlassCounter++;
+    glassCounter.innerHTML = currentGlassCounter;
+});
+
+
+removeButton.addEventListener('click', () => {
+
+    if (currentGlassCounter > 0) {
+        currentGlassCounter--;
+    }
+    glassCounter.innerHTML = currentGlassCounter;
+});
