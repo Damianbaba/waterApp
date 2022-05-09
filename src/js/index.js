b953d2740674165e0d8f305c1b9841e7b0b7694e
Damian Mroczek <damianmroczek@yahoo.com>
2022-05-09 15:11:50 +0200

    js localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -6,24 +6,35 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO Damian🚀')
 
 const glassCounter = document.querySelector('.glass__counter--js');
 const addButton = document.querySelector('.button-add--js');
 const removeButton = document.querySelector('.button-remove--js');
+const key = new Date().toLocaleString().slice(0, 10);
 
 let currentGlassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+    currentGlassCounter = localStorageValue;
+} else {
+    localStorage.setItem(key, currentGlassCounter);
+}
+
+glassCounter.innerHTML = currentGlassCounter;
 
 addButton.addEventListener('click', () => {
     currentGlassCounter++;
     glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
 });
 
-
 removeButton.addEventListener('click', () => {
-
     if (currentGlassCounter > 0) {
         currentGlassCounter--;
     }
     glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
 });
